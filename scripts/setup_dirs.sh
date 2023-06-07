86d9121e2f63b177eb770c596733117fbb50a50e
gitmurali <murali.nutalapatii@gmail.com>
2023-06-07 12:58:22 +0100

    update scripts

--- scripts/setup_dirs.sh
+++ scripts/setup_dirs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -xe
-mkdir -p /var/www/SampleApp
+mkdir -p /var/www/html/SampleApp
 chown nginx:nginx /var/www
-chown nginx:nginx /var/www/SampleApp
+chown nginx:nginx /var/www/html/SampleApp
